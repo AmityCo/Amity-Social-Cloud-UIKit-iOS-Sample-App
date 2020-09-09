17316446711a12e02b18e31ca574f79750edddd2
Nontapat Siengsanor <siengnontapat@gmail.com>
2020-09-09 16:14:24 +0700

    Add feature menu and settings page

---
+++ UpstraUIKitiOSSample/UpstraUIKitiOSSample/ViewControllers/MenuTableViewController.swift
@@ -0,0 +1,99 @@
+//
+//  MenuTableViewController.swift
+//  UpstraUIKitiOSSample
+//
+//  Created by Nontapat Siengsanor on 9/9/2563 BE.
+//  Copyright © 2563 Upstra. All rights reserved.
+//
+
+import UIKit
+import EkoChat
+import UpstraUIKit
+
+class MenuTableViewController: UITableViewController {
+    
+    private enum Menu: CaseIterable {
+        case chatHome
+        case chatList
+        case chatListCustomize
+        
+        var title: String {
+            switch self {
+            case .chatHome:
+                return "Chat Home"
+            case .chatList:
+                return "Chat List"
+            case .chatListCustomize:
+                return "Chat List with customization"
+            }
+        }
+    }
+    
+    // MARK: - View's life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: NSStringFromClass(UITableViewCell.self))
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Menu.allCases.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(UITableViewCell.self), for: indexPath)
+        cell.textLabel?.text = items(at: indexPath).title
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch items(at: indexPath) {
+        case .chatList:
+            navigateToChatList()
+        case .chatListCustomize:
+            navigateToChatListWithCustomization()
+        case .chatHome:
+            navigateToChatHome()
+        }
+    }
+    
+    // MARK: - Private functions
+    
+    private func items(at indexPath: IndexPath) -> Menu {
+        return Menu.allCases[indexPath.row]
+    }
+    
+    private func navigateToChatHome() {
+        let viewController = EkoChatHomePageViewController.make()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func navigateToChatList() {
+        let viewController = EkoMessageListViewController.make(channelId: "Global Channel")
+        viewController.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func navigateToChatListWithCustomization() {
+        var settings = EkoMessageListSettings()
+        settings.register(type: .textIncoming, cell: CustomTextIncomingCell.self)
+        settings.register(type: .imageOutgoing, cell: CustomTextIncomingCell.self)
+        
+        let viewController = EkoMessageListViewController.make(channelId: "Global Channel", settings: settings)
+        viewController.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+}
+
+class CustomTextIncomingCell: UITableViewCell, EkoMessageCellProtocol {
+    
+    func display(message: EkoMessagesModel, client: EkoClient) {
+        
+    }
+    
+}
---
+++ UpstraUIKitiOSSample/UpstraUIKitiOSSample/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsViewController.swift
+//  UpstraUIKitiOSSample
+//
+//  Created by Nontapat Siengsanor on 8/9/2563 BE.
+//  Copyright © 2563 Upstra. All rights reserved.
+//
+
+import UIKit
+import UpstraUIKit
+
+class SettingsViewController: UIViewController {
+    
+    private enum Preset {
+        case defaultPreset
+        case customPreset1
+        case customPreset2
+        
+        var theme: EkoTheme {
+            switch self {
+            case .defaultPreset:
+                return EkoTheme()
+            case .customPreset1:
+                return EkoTheme(primary: .brown, secondary: .black, alert: .red, highlight: .green, base: .gray, baseInverse: .white, messageBubble: .orange, messageBubbleInverse: .purple)
+            case .customPreset2:
+                return EkoTheme(primary: .systemBlue, secondary: .systemGray, alert: .systemRed, highlight: .systemGreen, base: .systemPink, baseInverse: .systemIndigo, messageBubble: .systemOrange, messageBubbleInverse: .systemTeal)
+            }
+        }
+    }
+    
+    // MARK: - View's life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func defaultPresetTap(_ sender: Any) {
+        let preset = Preset.defaultPreset
+        UpstraUIKit.set(theme: preset.theme)
+    }
+    
+    @IBAction func customPreset1Tap(_ sender: Any) {
+        let preset = Preset.customPreset1
+        UpstraUIKit.set(theme: preset.theme)
+    }
+    
+    @IBAction func customPreset2Tap(_ sender: Any) {
+        let preset = Preset.customPreset2
+        UpstraUIKit.set(theme: preset.theme)
+    }
+    
+}
